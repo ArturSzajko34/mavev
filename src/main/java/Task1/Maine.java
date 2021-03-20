62552f3e6435e0922f675f1a32446396975bd7a0
aszajko <aszajko25@gmail.com>
2021-03-20 10:10:09 +0100

    Task1

---
+++ src/main/java/Task1/Maine.java
@@ -0,0 +1,15 @@
+package Task1;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Maine {
+    public static void main(String[] args) {
+
+        System.out.println(StringUtils.isBlank(" "));
+
+
+    }
+    public static boolean verficationString(String value){
+        return value == null && value.length() > 0 && value.equals(value.trim());
+    }
+}
